0577d48f474ccd20f7f6fecdf3ba840765603aaf
chipslays <hello@chipslays.ru>
2021-02-05 14:54:59 +0400

    [feat] rename methods & add doc blocks

--- src/Crack.php
+++ src/Crack.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 namespace Cracker;
 
 use Intervention\Image\ImageManager;
 use thiagoalessio\TesseractOCR\TesseractOCR;
 
-class Crack 
+class Crack
 {
     private $image;
     private $manager;
@@ -18,6 +18,11 @@ class Crack
     private $tessdata = false;
     private $proxy = false;
 
+    /**
+     * Create Crack instance.
+     *
+     * @param string $image Can be File or Url
+     */
     public function __construct($image)
     {
         $this->manager = new ImageManager([
@@ -27,49 +32,92 @@ class Crack
         $this->file = $image;
     }
 
+    /**
+     * Set count of iteration. 
+     *
+     * @param string|int $count
+     * @return Crack
+     */
     public function iterations($count)
     {
-        $this->iterations = $count;
+        $this->iterations = (int) $count;
         return $this;
     }
 
-    public function storage($path)
+    /**
+     * Set writable folder for temp files.
+     *
+     * @param string $path
+     * @return Crack
+     */
+    public function temp($path)
     {
         $this->storage = rtrim($path, '\/');
         return $this;
     }
 
+    /**
+     * Set Tesseract lang (model).
+     *
+     * @param string $model
+     * @return Crack
+     */
     public function model($model = 'eng')
     {
         $this->model = $model;
         return $this;
     }
 
+    /**
+     * Set proxy.
+     * socks5://user:password@ip:port
+     *
+     * @param string $proxy
+     * @return Crack
+     */
     public function proxy($proxy)
     {
         $this->proxy = $proxy;
         return $this;
     }
 
-    public function ocr($executable = false)
+    /**
+     * Set executable Tesseract OCR.
+     *
+     * @param boolean $executable
+     * @return Crack
+     */
+    public function executable($executable = false)
     {
         $this->executable = $executable;
         return $this;
     }
-
-    public function data($tessdata = false)
+    
+    /**
+     * Set tessdata folder.
+     *
+     * @param string|boolean $tessdata
+     * @return Crack
+     */
+    public function tessdata($tessdata = false)
     {
         $this->tessdata = $tessdata;
         return $this;
     }
 
-    private function iteration() {
+    /**
+     * Convert image and OCR resolve.
+     *
+     * @return string|bool
+     */
+    private function iteration()
+    {
         if (is_file($this->file)) {
             $this->image = $this->manager->make($this->file);
         } else {
             if (!$this->proxy) {
                 $this->image = $this->manager->make($this->file);
-            } else{
+            } else {
                 $client = new \GuzzleHttp\Client();
 
                 try {
@@ -77,7 +125,7 @@ class Crack
                         'proxy' => $this->proxy,
                         'timeout' => 5,
                     ]);
-        
+
                     $this->image = $this->manager->make($res->getBody());
                 } catch (\Throwable $th) {
                     return false;
@@ -94,20 +142,26 @@ class Crack
             ->psm(7);
 
         if ($this->executable) {
-            $ocr->executable($this->executable); 
+            $ocr->executable($this->executable);
         }
 
         if ($this->tessdata) {
-            $ocr->tessdataDir($this->tessdata); 
+            $ocr->tessdataDir($this->tessdata);
         }
 
-        $text = $ocr->run(); 
+        $text = $ocr->run();
 
         unlink("{$this->storage}/resolve.gif");
 
         return trim($text) !== '' ? $text : false;
     }
 
+    /**
+     * Resolve captcha.
+     *
+     * @param boolean $returnArray Return array with detail info or only string of resolved captcha text.
+     * @return array|string 
+     */
     public function resolve($returnArray = false)
     {
         if (!$this->storage || !file_exists($this->storage)) {
@@ -116,7 +170,7 @@ class Crack
 
         $start = microtime(true);
 
-        for ($i=0; $i < $this->iterations; $i++) { 
+        for ($i = 0; $i < $this->iterations; $i++) {
             $result = $this->iteration();
 
             if (!$result) {
@@ -124,14 +178,14 @@ class Crack
             }
 
             $chars = array_filter(str_split(preg_replace('/\s+/', '', $result)));
-          
+
             $this->captchas['count'][] = count($chars);
 
             foreach ($chars as $key => $char) {
                 if (isset($this->captchas['chars'][$key]) && array_key_exists($char, $this->captchas['chars'][$key])) {
-                    $this->captchas['chars'][$key][$char]++; 
+                    $this->captchas['chars'][$key][$char]++;
                 } else {
-                    $this->captchas['chars'][$key][$char]= 1; 
+                    $this->captchas['chars'][$key][$char] = 1;
                 }
             }
         }
